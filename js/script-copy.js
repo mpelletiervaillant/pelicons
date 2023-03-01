c8adbc660ffe5283541ebbea7b4eff0c7457d93c
mpelletiervaillant <melany.pelletier@gmail.com>
2023-02-28 20:51:46 -0500

    Testing javascript

--- js/script-copy.js
+++ js/script-copy.js
@@ -1,11 +1,4 @@
 
-$(document).ready(function () {
-  $("a.show-message").on("click", function () {
-      console.log("clicked");
-      $(this).next().removeClass("hide");
-  });
-});
-
 // // Get the modal
 // var modal = document.getElementById("myModal");
 
@@ -32,3 +25,28 @@ $(document).ready(function () {
 //   }
 // }
 
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
